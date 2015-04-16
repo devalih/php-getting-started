7e7ef3463e1f4018497b13e512ab386a334ed9af
devalih <devalih@live.com>
2015-04-16 18:14:19 +0300

    home page

--- web/index.php
+++ web/index.php
@@ -14,7 +14,7 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return 'Hello';
+  return 'Testing Huruku محمد منسي';
 });
 
 $app->run();
